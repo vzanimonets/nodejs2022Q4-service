8616ed8d7844164188496781d01969730b36899f
Viktar Zanimonets <vzanimonets@gmail.com>
2023-02-06 19:24:19 +0300

    feat: resolve CoORS error

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT);
 }
