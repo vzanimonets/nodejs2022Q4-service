b29bd5e44d00d668bab065e01e2ebe174a7ba584
Viktar Zanimonets <vzanimonets@gmail.com>
2023-02-06 14:17:02 +0300

    hotfix: implementation of correct validation for  'track' endpoint

--- src/track/dto/create-track.dto.ts
+++ src/track/dto/create-track.dto.ts
@@ -1,9 +1,22 @@
-import { IsString } from 'class-validator';
+import {
+  IsDefined,
+  IsNumber,
+  IsString,
+  IsUUID,
+  ValidateIf,
+} from 'class-validator';
 
 export class CreateTrackDto {
   @IsString()
+  @IsDefined()
   name: string;
+  @ValidateIf((_, value) => !(value === null))
+  @IsUUID('4')
   artistId: string | null; // refers to Artist
+  @ValidateIf((_, value) => !(value === null))
+  @IsUUID('4')
   albumId: string | null; // refers to Album
+  @IsDefined()
+  @IsNumber()
   duration: number; // integer number
 }
